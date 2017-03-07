8593670f3a13b63e12982a190ca24ad52d7aee03
Layne Smith <layne@tf5kmc0dhf9vnd.ahc.belotechnologies.com>
2017-03-07 17:09:01 -0600

    fixing s3 links

--- build/js/builder.js
+++ build/js/builder.js
@@ -11,7 +11,7 @@ $(document).ready(() => {
   const randomImage = _.random(imagesArray.length - 1);
   console.log(randomImage);
 
-  const imageLink = `../images/base-images/${imagesArray[randomImage]}.jpg`;
+  const imageLink = `https://dmnmemebase.s3.amazonaws.com/${imagesArray[randomImage]}.jpg`;
   console.log(imageLink);
 
   // Stuff we'll use later
@@ -33,7 +33,7 @@ $(document).ready(() => {
   // LOAD THUMB IMAGE
   $('#thumbsBox img').click(function () {
     const targetImage = $(this).data('image');
-    const imageLink = `../images/base-images/${targetImage}.jpg`;
+    const imageLink = `https://dmnmemebaseresized.s3.amazonaws.com/resized-${targetImage}.jpg`;
     mountImage(imageLink);
   });
 
